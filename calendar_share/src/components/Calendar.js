902d93bab1aac7284975ff2202047e93c94ccbd5
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2023-05-08 02:16:45 -0400

    added seamless selection for selecting dates

--- calendar_share/src/components/Calendar.js
+++ calendar_share/src/components/Calendar.js
@@ -11,10 +11,9 @@ import { PickersDay } from '@mui/x-date-pickers/PickersDay';
 dayjs.extend(isBetweenPlugin);
 
 const CustomPickersDay = styled(PickersDay, {
-  shouldForwardProp: (prop) =>
-    prop !== 'dayIsSelected'
-})(({ theme, dayIsSelected }) => ({
-  ...(dayIsSelected && {
+  shouldForwardProp: (prop) => prop !== 'dayIsSelected',
+})(({ theme, dayIsSelected, isDragging }) => ({
+  ...(dayIsSelected && !isDragging && {
     borderRadius: 0,
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.common.white,
@@ -22,35 +21,26 @@ const CustomPickersDay = styled(PickersDay, {
       backgroundColor: theme.palette.primary.dark,
     },
   }),
+  ...(dayIsSelected && isDragging && {
+    backgroundColor: theme.palette.primary.light,
+  }),
 }));
 
 
-const dayList = [];
-
-function toggleDays(newValue){
-  console.log(newValue);
-
-  // find item in list and remove it if it exists 
-  const index = dayList.findIndex((d) => newValue.isSame(d, 'day'));
-  console.log('index: ' + index);
-  if (index !== -1){
-    dayList.splice(index, 1);
-  }
-  else{
-    dayList.push(newValue);   
-  }
-  
-  console.log(dayList);
-}
-
 function Day(props) {
-  const { day, selectedDay, setSelectedDays, ...other } = props;
+  const { day, selectedDay, dayList, isMouseDown, toggleDays, ...other } = props;
+  const dayIsSelected = dayList.some((d) => day.isSame(d, 'day'));
 
   if (selectedDay == null) {
     return <PickersDay day={day} {...other} />;
   }
 
-  const dayIsSelected = dayList.some((d) => day.isSame(d, 'day'));
+
+  const handleMouseOver = () => {
+    if (isMouseDown) {
+      toggleDays(day);
+    }
+  };
 
   return (
     <CustomPickersDay
@@ -58,6 +48,7 @@ function Day(props) {
       day={day}
       sx={dayIsSelected ? { px: 2.5, mx: 0 } : {}}
       dayIsSelected={dayIsSelected}
+      onMouseOver={handleMouseOver}
     />
   );
 }
@@ -71,24 +62,59 @@ Day.propTypes = {
 };
 
 export default function Calendar() {
-  const [value, setValue] = React.useState(dayjs('2022-05-06'));
+  const [value, setValue] = React.useState(dayjs('2023-05-06'));
+  const [isMouseDown, setIsMouseDown] = React.useState(false);
+  const [dayList, setDayList] = React.useState([]);
 
+
+  function toggleDays(newValue){
+    console.log(newValue);
+
+    // find item in list and remove it if it exists 
+    const index = dayList.findIndex((d) => newValue.isSame(d, 'day'));
+    console.log('index: ' + index);
+    if (index !== -1) {
+      const newList = [...dayList];
+      newList.splice(index, 1);
+      setDayList(newList);
+    } else {
+      setDayList([...dayList, newValue]);
+    }
+    
+    console.log(dayList);
+  }
+
+  const handleMouseDown = () => {
+    setIsMouseDown(true);
+  };
+  
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        showDaysOutsideCurrentMonth 
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue); 
-          toggleDays(newValue);
-        }}
-        slots={{ day: Day }}
-        slotProps={{
-          day: {
-            selectedDay: value,
-          },
-        }}
-      />
+      <div
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
+        <DateCalendar
+          showDaysOutsideCurrentMonth
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+            toggleDays(newValue);
+          }}
+          slots={{ day: Day }}
+          slotProps={{
+            day: {
+              selectedDay: value,
+              dayList,
+              isMouseDown,
+              toggleDays,
+            },
+          }}
+        />
+      </div>
     </LocalizationProvider>
   );
 }
